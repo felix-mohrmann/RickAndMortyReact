0d3a4441c9e2bfdd33527dd388ee526c527c8cb2
felix-mohrmann <mohrmann.felix@gmail.com>
2021-08-04 17:26:26 +0200

    Add Character Cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,8 @@
 import './CharacterCard.css'
 
-export default function CharacterCard(){
-    return null;
+export default function CharacterCard(props) {
+    return <section className="character-card">
+        <img className="character-card__img" src={props.character.image} alt="character img"/>
+        <h2 className="character-card__title">{props.character.name}</h2>
+    </section>
 }
